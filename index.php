e1b9ead01a8b8502df111f3d8a6afc7c08e06c40
Rafaeldovale <70954053+Rafaeldovale@users.noreply.github.com>
2021-02-13 10:54:11 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+
+
+        <form action = "dados.php" method = "POST">
+            Nome: <input type = "text" name = "nome"><br>
+            Email: <input type = "text" name = "email"><br>
+            <input type = "submit" name = "enviar">
+        </form>
+    </body>
+</html> 
